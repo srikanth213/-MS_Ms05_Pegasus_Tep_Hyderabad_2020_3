ba7609781c389361840a75069ec66b4c3b071a82
srikanth213 <61818927+srikanth213@users.noreply.github.com>
2020-04-21 15:25:03 +0530

    background file added
    background file added

---
+++ background.js
@@ -0,0 +1,36 @@
+
+/* global chrome */
+/* global URL */
+/* global tabs  */
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({
+    'enabled': true,
+    'blocked': [
+      'facebook.com',
+      'instagram.com',
+      'youtube.com',
+      'twitter.com',
+      ],
+    'tabs': {}
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+  if (!tabId || !changeInfo.url || !changeInfo.url.startsWith('http')) {
+    return;
+  }
+
+  chrome.storage.sync.get(['enabled', 'tabs'], function (result) {
+    // Store the tab
+    try {
+      result.tabs[tabId] = new URL(changeInfo.url).hostname;
+      chrome.storage.sync.set({ 'tabs': result.tabs }, function() {
+        if (result.enabled) {
+          chrome.extension.getBackgroundPage().removeTabs();
+        }
+      });
+    }
+    catch (err) {}
+  });
+});
